ccef7afaaa3bc1b2c057d304662af8e3e15ca0c9
jbrown7407 <jbrown7407@gmail.com>
2020-09-28 17:57:44 -0400

    IDE
    IDE

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function run() {
-    let htmlCode = document.querySelector(".editor #html").value;
+    let htmlCode = document.querySelector(".editor #html").value; //store editor content as variables
     let cssCode = "<style>"+document.querySelector(".editor #css").value+"</style>";
     let jsCode = document.querySelector(".editor #js").value;
-    let output = document.querySelector("#output");
-    //console.log(htmlCode,cssCode,jsCode,output);
-    output.contentDocument.body.innerHTML = htmlCode+cssCode;
-    output.contentWindow.eval(jsCode);
+    let output = document.querySelector("#output"); // Store output div as variable
+  
+    output.contentDocument.body.innerHTML = htmlCode+cssCode; //Link to CSS + HTML
+    output.contentWindow.eval(jsCode);    //Link to in-editor script
 }
-document.querySelector(".editor #html").addEventListener("keyup",run);
-document.querySelector(".editor #css").addEventListener("keyup",run);
-document.querySelector(".editor #js").addEventListener("keyup",run);
+document.querySelector(".editor #html").addEventListener("keyup",run); //update html in editor each press by calling run()
+document.querySelector(".editor #css").addEventListener("keyup",run);  //update css in editor each press by calling run()
+document.querySelector(".editor #js").addEventListener("keyup",run);   //update js in editor each press by calling run()
