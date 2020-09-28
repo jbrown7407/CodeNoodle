1844d5ef4112ca08a0087ff926ec4cbbc5e9cf07
jbrown7407 <jbrown7407@gmail.com>
2020-09-28 17:06:42 -0400

    x

---
+++ app.js
@@ -0,0 +1,12 @@
+function run() {
+    let htmlCode = document.querySelector(".editor #html").value;
+    let cssCode = "<style>"+document.querySelector(".editor #css").value+"</style>";
+    let jsCode = document.querySelector(".editor #js").value;
+    let output = document.querySelector("#output");
+    //console.log(htmlCode,cssCode,jsCode,output);
+    output.contentDocument.body.innerHTML = htmlCode+cssCode;
+    output.contentWindow.eval(jsCode);
+}
+document.querySelector(".editor #html").addEventListener("keyup",run);
+document.querySelector(".editor #css").addEventListener("keyup",run);
+document.querySelector(".editor #js").addEventListener("keyup",run);
