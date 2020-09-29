e9a444d71663dee93586bd5159ef03272624e4b4
jbrown7407 <jbrown7407@gmail.com>
2020-09-28 20:38:22 -0400

    Run code button
    x

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 function run() {
     let htmlCode = document.querySelector(".editor #html").value; //store editor content as variables
     let cssCode = "<style>"+document.querySelector(".editor #css").value+"</style>";
-    let jsCode = document.querySelector(".editor #js").value;
+  //  let jsCode = document.querySelector(".editor #js").value;
     let output = document.querySelector("#output"); // Store output div as variable
   
     output.contentDocument.body.innerHTML = htmlCode+cssCode; //Link to CSS + HTML
-    output.contentWindow.eval(jsCode);    //Link to in-editor script
+  //  output.contentWindow.eval(jsCode);    //Link to in-editor script
 }
+
+function onClick() {
+    let jsCode = document.querySelector(".editor #js").value;
+    output.contentWindow.eval(jsCode); 
+}
+
+
 document.querySelector(".editor #html").addEventListener("keyup",run); //update html in editor each press by calling run()
 document.querySelector(".editor #css").addEventListener("keyup",run);  //update css in editor each press by calling run()
-document.querySelector(".editor #js").addEventListener("keyup",run);   //update js in editor each press by calling run()/
+//document.querySelector(".point").addEventListener("onClick",run);   //update js only onClick()/
