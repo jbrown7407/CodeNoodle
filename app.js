d4a7bf2e38858c7bc0f2a6241400317c69600350
jbrown7407 <jbrown7407@gmail.com>
2020-10-07 21:37:01 -0400

    1.2 Change Viewports to preview mobile

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 //CREATE NEW FILE CODE
 
 
+const red = Math.floor(Math.random() * 256);
+const green = Math.floor(Math.random() * 256);
+const blue = Math.floor(Math.random() * 256);
+const color = 'rgb('+red+','+green+','+blue+')';
 
 
+
+//ORIGINAL
+// function run() {
+//   let htmlCode = document.querySelector(".editor #html").value; //store editor content as variables
+//   let cssCode = "<style>"+document.querySelector(".editor #css").value+"</style>";
+// //  let jsCode = document.querySelector(".editor #js").value;
+//   let output = document.querySelector("#output"); // Store output div as variable
+//   output.contentDocument.body.innerHTML = htmlCode+cssCode; //Link to CSS + HTML
+// //  output.contentWindow.eval(jsCode);    //Link to in-editor script
+// }
 
 function run() {
-  let htmlCode = document.querySelector(".editor #html").value; //store editor content as variables
-  let cssCode = "<style>"+document.querySelector(".editor #css").value+"</style>";
-//  let jsCode = document.querySelector(".editor #js").value;
-  let output = document.querySelector("#output"); // Store output div as variable
-  output.contentDocument.body.innerHTML = htmlCode+cssCode; //Link to CSS + HTML
-//  output.contentWindow.eval(jsCode);    //Link to in-editor script
+    let htmlCode = document.querySelector(".editor #html").value; //store editor content as variables
+
+    document.body.style.wordWrap = 'break-word';// IFRAME LINES
+
+    let cssCode = "<style>"+document.querySelector(".editor #css").value+"</style>";
+  //  let jsCode = document.querySelector(".editor #js").value;
+    let output = document.querySelector("#output"); // Store output div as variable
+    $(output).addClass("hotText")
+    output.contentDocument.body.innerHTML = htmlCode+cssCode; //Link to CSS + HTML
+   // iframe.contentWindow.document.body.style.wordWrap = 'break-word';
+  //   iframe.height = iframe.contentWindow.document.body.scrollHeight + 'px';
 }
+  //  output.contentWindow.eval(jsCode);    //Link to in-editor script
+  output.contentDocument.body.style.wordWrap = 'break-word';
+
+
 function onClick() {
   let jsCode = document.querySelector(".editor #js").value;
   output.contentWindow.eval(jsCode);  //Run JS in editor
 }
+
+
 
 
 
@@ -71,6 +96,7 @@ function copyThisHTML() {  //COPIES HTML TO CLIPBOARD
         console.log('saving!')
     };
     }());
+
     let htmlCode = document.querySelector(".editor #html").value;
          var data = { value : 33, htmlCode}, // THIS OBJECT IS PASSED
          fileName = "passed.txt";
@@ -90,7 +116,10 @@ function copyThisHTML() {  //COPIES HTML TO CLIPBOARD
 
 
   
-  $(function() {  //JQUERY
+  //JQUERY
+
+
+
 
 let cl = (value) => {console.log(value)}
 
@@ -102,7 +131,7 @@ let cl = (value) => {console.log(value)}
   $("#btn2").hide()
 
 
- 
+ //PREVIEW MODE
    $("#btn2").click(function restore(){ //RETURN TO EDITOR
  
     $(".phase1").fadeIn(500).show();
@@ -113,7 +142,7 @@ let cl = (value) => {console.log(value)}
     cl("Title Screen")
        });
 let counterx = 1
-  $("#btn1").click(function fullScreen(){ //Full Screen
+  $("#btn1").click(function fullScreen(){ //Full ScreenMode
         $(".phase3").hide()
         $(".phase1").slideUp(400).hide();
         $(".phase4").hide()
@@ -124,9 +153,193 @@ let counterx = 1
         counterx++
         $("#btn1").hide()
         $("#btn2").show()
-        
+  })
 
 
 
 
-          })})
+const $colorPalette = $('#color-palette');
+const $myPalette = $('#my-palette');
+const $generate = $('#generate');
+
+
+
+const makePalette = () => {
+  $colorPalette.empty()
+  for(let i = 0; i < 10; i++){
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const color = 'rgb('+red+','+green+','+blue+')';
+
+    const $square = $('<div>');
+    $square.addClass('square')
+    $square.css('background-color', color);
+    $colorPalette.append($square);
+    console.log("making palette")
+  }
+}
+makePalette()
+
+$($generate).click(function generate(){ //TITLE SCREEN
+  $colorPalette.empty()
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const color = 'rgb('+red+','+green+','+blue+')';
+
+  const $square = $('<div>');
+  $square.addClass('square')
+  $square.css('background-color', color);
+  $colorPalette.append($square);
+
+    $myPalette.show()
+    // $colorPalette.hide()
+    $myPalette.text("Color Red:" + red + " ,Color Blue:" + blue +" ,Color Green:" + green)
+
+    console.log("replace");
+    
+      })
+   
+   
+   
+      let $deviceName = document.getElementById('deviceName')
+
+
+
+    //RESIZE PREVIEW
+  let monitorsize =document.getElementsByClassName("monitorSize")
+  $monitorsize= monitorsize
+      function resizePreview() {
+
+        // $(event.currentTarget).css('color', '100%')
+        $('#output').css('height', '200px')
+     $('iframe').css('overflow', 'break-word')
+     $('iframe').css('wordwrap', 'break-word')
+        $('iframe').css('height', '640px')
+        $('iframe').css('width', '360px')
+        $('iframe').css('wordwrap', 'break-word')
+       
+        $('#deviceName').text(" Galaxy S5")
+       
+         console.log("resize Galaxy S5")
+      }
+      function resizePreview2() {
+        $('#output').css('height', '200px')
+        $('iframe').css('overflow', 'break-word')
+        $('iframe').css('wordwrap', 'break-word')
+           $('iframe').css('height', '411px')
+           $('iframe').css('width', '731px')
+           $('iframe').css('wordwrap', 'break-word')
+           $('#deviceName').text(" Pixel 2")
+          
+            console.log("resize Pixel2")
+       }
+       function resizePreview3() {
+        $('#output').css('height', '200px')
+        $('iframe').css('overflow', 'break-word')
+        $('iframe').css('wordwrap', 'break-word')
+           $('iframe').css('height', '411px')
+           $('iframe').css('width', '823px')
+           $('iframe').css('wordwrap', 'break-word')
+           $('#deviceName').text(" Pixel 2 XL")
+          
+            console.log("resize Pixel 2 XL")
+       }
+       function resizePreview1() {
+        $('#output').css('height', '200px')
+        $('iframe').css('overflow', 'break-word')
+        $('iframe').css('wordwrap', 'break-word')
+           $('iframe').css('height', '667px')
+           $('iframe').css('width', '375px')
+           $('iframe').css('wordwrap', 'break-word')
+           $('#deviceName').text(" iPhone 6/7/8")
+          
+            console.log("resize iPhone 6/7/8")
+       }
+       function resizePreview4() {
+        $('#output').css('height', '200px')
+        $('iframe').css('overflow', 'break-word')
+        $('iframe').css('wordwrap', 'break-word')
+           $('iframe').css('height', '736px')
+           $('iframe').css('width', '414px')
+           $('iframe').css('wordwrap', 'break-word')
+           $('#deviceName').text(" iPhone 6/7/8+")
+          
+        console.log("resize iPhone 6/7/8+")
+       }
+       function resizePreview5() {
+        $('#output').css('height', '200px')
+        $('iframe').css('overflow', 'break-word')
+        $('iframe').css('wordwrap', 'break-word')
+           $('iframe').css('height', '812px')
+           $('iframe').css('width', '375px')
+           $('iframe').css('wordwrap', 'break-word')
+           $('#deviceName').text(" iPhone X")
+        console.log("Iphone X")
+       }
+       function resizePreview6() {
+        console.log("Ipad")
+        $('#output').css('height', '200px')
+        $('iframe').css('overflow', 'break-word')
+        $('iframe').css('wordwrap', 'break-word')
+           $('iframe').css('height', '1024px')
+           $('iframe').css('width', '768px')
+           $('iframe').css('wordwrap', 'break-word')
+           $('#deviceName').text(" iPad")
+       }
+       function resizePreview7() {
+        $('#output').css('height', '200px')
+        $('iframe').css('overflow', 'break-word')
+        $('iframe').css('wordwrap', 'break-word')
+           $('iframe').css('height', '1366px')
+           $('iframe').css('width', '1024px')
+           $('iframe').css('wordwrap', 'break-word')
+        console.log("resize Ipad Pro")
+        $('#deviceName').text(" iPad Pro")
+       }
+       function resizePreview8() {
+        $('#output').css('height', '200px')
+        $('iframe').css('overflow', 'break-word')
+        $('iframe').css('wordwrap', 'break-word')
+           $('iframe').css('height', '720px')
+           $('iframe').css('width', '540px')
+           $('iframe').css('wordwrap', 'break-word')
+           $('#deviceName').text(" Surface Duo")
+        console.log("resize Surface Duo")
+       }
+       function resizePreview9() {
+        $('#output').css('height', '280px')
+        $('iframe').css('overflow', 'break-word')
+        $('iframe').css('wordwrap', 'break-word')
+           $('iframe').css('height', '653px')
+           $('iframe').css('width', '280px')
+           $('iframe').css('wordwrap', 'break-word')
+           $('#deviceName').text(" Galaxy Fold")
+        console.log("resize Galaxy Fold")
+       }
+       function resizePreview10() {
+        $('#output').css('height', '1080px')
+        $('iframe').css('overflow', 'break-word')
+        $('iframe').css('wordwrap', 'break-word')
+           $('iframe').css('height', '1080px')
+           $('iframe').css('width', '1920px')
+           $('iframe').css('wordwrap', 'break-word')
+           $('#deviceName').text(" 1920x1080")
+        console.log(" 1920x1080")
+       }
+       function resizePreview11() {
+        $('#output').css('height', '1080px')
+        $('iframe').css('overflow', 'break-word')
+        $('iframe').css('wordwrap', 'break-word')
+           $('iframe').css('height', '800px')
+           $('iframe').css('width', '1280px')
+           $('iframe').css('wordwrap', 'break-word')
+           $('#deviceName').text(" 1200x800 (13-inch)")
+        console.log(" 13 inch")
+       }
+
+
+       resizePreview11()
+
+          
